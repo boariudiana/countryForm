1d6a26df520b06f3ca12a5dfaf0be6261cbe2a0a
Diana Boariu <boariu.diana11@gmail.com>
2025-04-05 23:39:50 +0200

    change configs

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,4 +6,8 @@ const env: EnvConfig = {
   GEONAMES_USERNAME: import.meta.env.VITE_GEONAMES_USERNAME || ''
 };
 
+if (!env.GEONAMES_USERNAME) {
+  console.error('VITE_GEONAMES_USERNAME is not set in .env file');
+}
+
 export default env; 
